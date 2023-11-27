34be1b5b57364d8b957e71c933996d563057dfaf
devalexsegault <alexandresegault.dev@gmail.com>
2023-11-27 11:56:09 +0100

    chap_1

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+class TodoTile extends StatelessWidget {
+  const TodoTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container();
   }
 }
